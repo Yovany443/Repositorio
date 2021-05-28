6e145b913ac90d580f02fe6681b158081e9d2224
Yovany443 <84158987+Yovany443@users.noreply.github.com>
2021-05-28 15:58:06 -0500

    Add files via upload

---
+++ ACTIVIDAD_2/A.py
@@ -0,0 +1,5 @@
+#realizado por Yovany Ferney Toro Botero
+r = int(input("ingrese un numero entero"))
+for i in range(r+1):
+    r = list(range(i, -1,))
+    print(i)
---
+++ ACTIVIDAD_2/B.py
@@ -0,0 +1,4 @@
+#realizado por Yovany Ferney Toro Botero
+r = (input("ingrese el nombre de la fruta: "))
+for i in r:
+    print(i)
